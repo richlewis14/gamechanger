9e992c6dd27dd0f3791645f309716b926570aee1
Richard Lewis <richlewis14@gmail.com>
2018-04-21 14:28:09 +0100

    Adding X-Content-Type-Options header

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ app.use(flash());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("X-Content-Type-Options", "nosniff");
   next();
 });
 
